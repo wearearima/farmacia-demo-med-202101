44cd86503835167c9ea665b0302799d6f643cc1a
jessica <jessica@arima.eu>
2021-01-19 14:50:51 +0100

    proyecto creado con spring initializer (https://start.spring.io/)

---
+++ src/test/java/eu/arima/mejorarTesting/farmacia/FarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.arima.mejorarTesting.farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
